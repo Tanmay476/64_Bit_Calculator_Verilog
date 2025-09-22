84f2a6facbeac758e49b13ebff2ba38d5ea0e909
tanmay476 <tanmay4706@gmail.com>
2025-09-22 18:13:38 -0400

    Adding Verilog Files

---
+++ verilog/calc_sb.svh
@@ -0,0 +1,36 @@
+class calc_sb #(int DataSize, int AddrSize);
+
+  // Signals needed for the golden model implementation in the scoreboard
+  int mem_a [2**AddrSize];
+  int mem_b [2**AddrSize];
+  logic second_read = 0;
+  int golden_lower_data;
+  int golden_upper_data;
+  mailbox #(calc_seq_item #(DataSize, AddrSize)) sb_box;
+
+  function new(mailbox #(calc_seq_item #(DataSize, AddrSize)) sb_box);
+    this.sb_box = sb_box;
+  endfunction
+
+  task main();
+    calc_seq_item #(DataSize, AddrSize) trans;
+    forever begin
+      sb_box.get(trans);
+      // TODO: Implement the scoreboard's core functionality.
+      // The scoreboard's task is to verify the DUT's behavior by comparing the
+      // data received from the monitor against a golden reference model.
+
+      // Use the transaction flags (`initialize`, `rdn_wr`) to handle three distinct operations:
+      // - For initialization, update the scoreboard's local memory (`mem_a` and `mem_b`) to match the DUT's initial SRAM state.
+      // - For read operations, compare the data from the SRAM in the DUT to the data stored in the scoreboard's memory.
+      //       Think about how to account for the two sequential reads in the DUT for the single write operation. The values
+      //       from both read operations need to be used to compare against the calculated values in the DUT when they are written
+      //       to SRAM. The second_read, golden_lower_data, and golden_upper_data signals can be used for this purpose.
+      // - For write operations, compare the DUT's output to the data calculated by the golden model in the scoreboard.
+
+      // Use `$display` to log successful transactions and `$error` to report mismatches.
+      // If a mismatch occurs, use `$finish` to terminate the simulation.
+    end
+  endtask
+
+endclass : calc_sb
