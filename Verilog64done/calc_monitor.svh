67c596046a0e17bec79de4e4476c680ba9d944b1
Tanmay Shukla <tanmay4706@gmail.com>
2025-09-26 14:59:19 -0400

    Refactor transaction sampling in calc_monitor.svh
    Updated transaction handling in calc_monitor.svh to correctly sample and assign data for write and read operations from the clocking block. Added initialization transaction handling with appropriate fields.

--- Verilog64done/calc_monitor.svh
+++ Verilog64done/calc_monitor.svh
@@ -18,12 +18,22 @@ class calc_monitor #(int DataSize, int AddrSize);
       // Sample the transaction and send to scoreboard
       if (calcVif.cb.wr_en || calcVif.cb.rd_en) begin
         calc_seq_item #(DataSize, AddrSize) trans = new();
-        // TODO: Assign all values in the "trans" sequence item object with relevant signals from the clocking block
+        // Capture common fields visible at this clocking sample
+        trans.rdn_wr       = calcVif.cb.wr_en; // write=1, read=0
+        trans.curr_wr_addr = calcVif.cb.curr_wr_addr;
+        trans.curr_rd_addr = calcVif.cb.curr_rd_addr;
+        trans.read_start_addr  = calcVif.cb.read_start_addr;
+        trans.read_end_addr    = calcVif.cb.read_end_addr;
+        trans.write_start_addr = calcVif.cb.write_start_addr;
+        trans.write_end_addr   = calcVif.cb.write_end_addr;
+        trans.loc_sel      = calcVif.cb.loc_sel;
 
         if (trans.rdn_wr) // Write
         begin
-          // TODO: Assign the data for the transaction from the clocking block correctly
-          
+          // Sample write data (packed {upper, lower}) from clocking block
+          trans.lower_data = calcVif.cb.wr_data[31:0];
+          trans.upper_data = calcVif.cb.wr_data[63:32];
+
           if (!written) begin
             written = 1;
             $display($stime, " Mon: Write to Addr: 0x%0x, Data to SRAM A (lower 32 bits): 0x%0x, Data to SRAM B (upper 32 bits): 0x%0x\n",
@@ -33,21 +43,28 @@ class calc_monitor #(int DataSize, int AddrSize);
         end
         else if (!trans.rdn_wr) // Read
         begin
+          // For reads, r_data may become valid the following clocking sample in this design
           @(calcVif.cb);
           written = 0;
-          // TODO: Assign the data for the transaction from the clocking block correctly
+          // Sample read data (packed {upper, lower}) from clocking block
+          trans.lower_data = calcVif.cb.rd_data[31:0];
+          trans.upper_data = calcVif.cb.rd_data[63:32];
 
           $display($stime, " Mon: Read from Addr: 0x%0x, Data from SRAM A: 0x%0x, Data from SRAM B: 0x%0x\n",
-              trans.curr_rd_addr, trans.upper_data, trans.lower_data);
+              trans.curr_rd_addr, trans.lower_data, trans.upper_data);
           mon_box.put(trans);
         end
       end
 
       if (calcVif.cb.initialize) begin
         calc_seq_item #(DataSize, AddrSize) trans = new();
-        // TODO: Assign the right fields for the transaction from the clocking block signals that are
-        // relevant to initializing SRAM
-        // HINT: How do you differentiate which data belongs to which SRAM block?
+        // Populate init transaction fields so the scoreboard can update mirrors
+        trans.initialize = calcVif.cb.initialize;
+        trans.initialize_addr = calcVif.cb.initialize_addr;
+        trans.initialize_data = calcVif.cb.initialize_data;
+        trans.loc_sel = calcVif.cb.initialize_loc_sel;
+        // For clarity mark as a write-like transaction
+        trans.rdn_wr = 1;
 
         $display($stime, " Mon: Initialize SRAM; Write to SRAM %s, Addr: 0x%0x, Data: 0x%0x\n", !calcVif.cb.initialize_loc_sel ? "A" : "B", calcVif.cb.initialize_addr, calcVif.cb.initialize_data);
         mon_box.put(trans);
