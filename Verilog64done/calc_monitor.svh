acb0ab33f268893079652bbe842bd446831476b7
Tanmay Shukla <tanmay4706@gmail.com>
2025-09-26 15:10:42 -0400

    Refactor calc_monitor.svh for clarity and efficiency

--- Verilog64done/calc_monitor.svh
+++ Verilog64done/calc_monitor.svh
@@ -18,22 +18,20 @@ class calc_monitor #(int DataSize, int AddrSize);
       // Sample the transaction and send to scoreboard
       if (calcVif.cb.wr_en || calcVif.cb.rd_en) begin
         calc_seq_item #(DataSize, AddrSize) trans = new();
-        // Capture common fields visible at this clocking sample
-        trans.rdn_wr       = calcVif.cb.wr_en; // write=1, read=0
-        trans.curr_wr_addr = calcVif.cb.curr_wr_addr;
-        trans.curr_rd_addr = calcVif.cb.curr_rd_addr;
-        trans.read_start_addr  = calcVif.cb.read_start_addr;
-        trans.read_end_addr    = calcVif.cb.read_end_addr;
+        // Common fields sampled from the clocking block
+        trans.loc_sel = calcVif.cb.loc_sel;
+        trans.read_start_addr = calcVif.cb.read_start_addr;
+        trans.read_end_addr   = calcVif.cb.read_end_addr;
         trans.write_start_addr = calcVif.cb.write_start_addr;
         trans.write_end_addr   = calcVif.cb.write_end_addr;
-        trans.loc_sel      = calcVif.cb.loc_sel;
 
-        if (trans.rdn_wr) // Write
-        begin
-          // Sample write data (packed {upper, lower}) from clocking block
-          trans.lower_data = calcVif.cb.wr_data[31:0];
-          trans.upper_data = calcVif.cb.wr_data[63:32];
-
+        // Distinguish between write and read based on the clocking sampled enables
+        if (calcVif.cb.wr_en) begin
+          trans.rdn_wr = 1; // write
+          trans.curr_wr_addr = calcVif.cb.curr_wr_addr;
+          // wr_data is 2*DataSize wide: upper half then lower half
+          trans.lower_data = calcVif.cb.wr_data[DataSize-1:0];
+          trans.upper_data = calcVif.cb.wr_data[DataSize*2-1:DataSize];
           if (!written) begin
             written = 1;
             $display($stime, " Mon: Write to Addr: 0x%0x, Data to SRAM A (lower 32 bits): 0x%0x, Data to SRAM B (upper 32 bits): 0x%0x\n",
@@ -41,14 +39,16 @@ class calc_monitor #(int DataSize, int AddrSize);
             mon_box.put(trans);
           end
         end
-        else if (!trans.rdn_wr) // Read
-        begin
-          // For reads, r_data may become valid the following clocking sample in this design
+        else begin // read
+          // For reads, capture the address now (when rd_en is seen) and sample data on the
+          // following clock so we bind the data to the address that was requested.
+          logic [AddrSize-1:0] sampled_addr = calcVif.cb.curr_rd_addr;
           @(calcVif.cb);
+          trans.rdn_wr = 0; // read
           written = 0;
-          // Sample read data (packed {upper, lower}) from clocking block
-          trans.lower_data = calcVif.cb.rd_data[31:0];
-          trans.upper_data = calcVif.cb.rd_data[63:32];
+          trans.curr_rd_addr = sampled_addr;
+          trans.lower_data = calcVif.cb.rd_data[DataSize-1:0];
+          trans.upper_data = calcVif.cb.rd_data[DataSize*2-1:DataSize];
 
           $display($stime, " Mon: Read from Addr: 0x%0x, Data from SRAM A: 0x%0x, Data from SRAM B: 0x%0x\n",
               trans.curr_rd_addr, trans.lower_data, trans.upper_data);
@@ -58,14 +58,10 @@ class calc_monitor #(int DataSize, int AddrSize);
 
       if (calcVif.cb.initialize) begin
         calc_seq_item #(DataSize, AddrSize) trans = new();
-        // Populate init transaction fields so the scoreboard can update mirrors
-        trans.initialize = calcVif.cb.initialize;
+        trans.initialize = 1;
         trans.initialize_addr = calcVif.cb.initialize_addr;
         trans.initialize_data = calcVif.cb.initialize_data;
         trans.loc_sel = calcVif.cb.initialize_loc_sel;
-        // For clarity mark as a write-like transaction
-        trans.rdn_wr = 1;
-
         $display($stime, " Mon: Initialize SRAM; Write to SRAM %s, Addr: 0x%0x, Data: 0x%0x\n", !calcVif.cb.initialize_loc_sel ? "A" : "B", calcVif.cb.initialize_addr, calcVif.cb.initialize_data);
         mon_box.put(trans);
       end
